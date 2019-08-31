f15db11a421dfa164a26a18996e93d47ed76243b
jernicozz <cendleader@gmail.com>
2019-08-31 11:44:44 +0300

    console output

--- app.js
+++ app.js
@@ -1,32 +1,32 @@
-let http    = require("http")
+let http    = require("http");
 let https = require('https');
-let launcher = require("child_process")
-let fs      = require("fs")
-let express = require("express")
-var options = {
+let launcher = require("child_process");
+let fs      = require("fs");
+let express = require("express");
+/*const options = {
     key: fs.readFileSync('../key.pem'),
     cert: fs.readFileSync('../cert.pem')
-}
-  
-const app = express()
-app.use(express.static(__dirname))
+};*/
+const app = express();
+app.use(express.static(__dirname));
 app.use("/exec", (req, res)=>{
-    var content = ""
+    let content = "";
     req.on("data", ch=>{
         content += ch
-    })
+    });
     req.on("end", ()=>{
-        fs.writeFileSync("main.dul", content)
-        let process = launcher.spawn(`/root/Dulang/dulang` ,[`launch`, `${__dirname}/main.dul`])
-        setTimeout(()=>{
-            res.send(process.stdout.read())
-        }, 5000)
+        fs.writeFileSync("main.dul", content);
+        launcher.exec(`/Users/jernicozz/Dulang/dulang launch ${__dirname}/main.dul`, (_, stdout, __)=>{
+            console.log(_);
+            console.log(__);
+            res.send(stdout);
+        });
     })
     
-})
+});
 app.use("/", (_, res)=>{
     res.sendFile(__dirname+"/landing.html")
-})
+});
 
-http.createServer(app).listen(80)
-https.createServer(options, app).listen(443)
+http.createServer(app).listen(80);
+//https.createServer(options, app).listen(443);
--- static/editor.js
+++ static/editor.js
@@ -1,42 +1,42 @@
-let editor = document.querySelector("#coder")
+let editor = document.querySelector("#coder");
 editor.onkeydown = (e)=>{
-    let keyCode = e.keyCode || e.which
-    if (keyCode == 9) {
-        e.preventDefault()
-        let start = editor.selectionStart
-        let end = editor.selectionEnd
+    let keyCode = e.keyCode || e.which;
+    if (keyCode === 9) {
+        e.preventDefault();
+        let start = editor.selectionStart;
+        let end = editor.selectionEnd;
         editor.value = editor.value.substring(0, start)
                 + "\t"
-                + editor.value.substring(end)
+                + editor.value.substring(end);
 
         editor.selectionStart =
         editor.selectionEnd = start + 1
     }
-}
-let bmenu = document.querySelector(".bwrapper")
-let boast = document.querySelector(".boast_selector")
+};
+let bmenu = document.querySelector(".bwrapper");
+let boast = document.querySelector(".boast_selector");
 boast.onmouseover = ()=>{
     bmenu.style.display = "flex"
-}
+};
 
 let arguments_ = [
     ["", "no explicit self", "consize lambdas", "convinient singletons"],
     ["", "appropriate this", "no let-var-const", "no voodoo type casting", "async keyword"],
     ["", "up to 3 times less code", "convinient singletons", "easy async", "dynamic object semi-prototyped inheritance"]
-]
-let base_arg = document.querySelector("#barg")
-let selectors = document.querySelectorAll(".boast_selector")
+];
+let base_arg = document.querySelector("#barg");
+let selectors = document.querySelectorAll(".boast_selector");
 for(let i = 0; i<arguments_.length; ++i){
     selectors[i].onmouseover = ()=>{
         selectors.forEach(e=>{
             e.removeAttribute("selected")
-        })
-        selectors[i].setAttribute("selected", "true")
+        });
+        selectors[i].setAttribute("selected", "true");
         base_arg.innerHTML = arguments_[i].reduce(
             (acc, elem)=>{
-                let node = document.createElement("div")
-                node.classList.add("boast_arg")
-                node.innerHTML = elem
+                let node = document.createElement("div");
+                node.classList.add("boast_arg");
+                node.innerHTML = elem;
                 return acc + node.outerHTML
             }
         )
@@ -45,17 +45,17 @@ for(let i = 0; i<arguments_.length; ++i){
 
 document.querySelector("#comm").onclick = ()=>{
     window.location.href = "https://vk.com/dulangdev"
-}
+};
 document.querySelector("#togit").onclick = ()=>{
     window.location.href = "https://github.com//Orfund/dulang"
-}
+};
 
 document.querySelector(".syntax_demo > svg").onclick = ()=>{
-    let xhr = new XMLHttpRequest()
-    xhr.open("POST", "/exec")
-    xhr.setRequestHeader("Content-type", "text-plain")
+    let xhr = new XMLHttpRequest();
+    xhr.open("POST", "/exec");
+    xhr.setRequestHeader("Content-type", "text-plain");
     xhr.onload = ()=>{
-        alert(xhr.responseText)
-    }
+        document.querySelector(".output").value = xhr.responseText
+    };
     xhr.send(document.querySelector(".syntax_demo textarea").value)
-}
+};
