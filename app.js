754039a6852de7ae1a797ac49a2b1123eb02941e
jernicozz <cendleader@gmail.com>
2019-08-31 04:07:16 +0300

    https

--- app.js
+++ app.js
@@ -1,10 +1,15 @@
 let http    = require("http")
 let fs      = require("fs")
 let express = require("express")
-
+var options = {
+    key: fs.readFileSync('../key.pem'),
+    cert: fs.readFileSync('../cert.pem')
+}
+  
 const app = express()
 app.use(express.static(__dirname))
 app.use("/", (_, res)=>{
     res.sendFile(__dirname+"/landing.html")
 })
-app.listen(3000)
+app.listen(80)
+https.createServer(options, app).listen(443)
